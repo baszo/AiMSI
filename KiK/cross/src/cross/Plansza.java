f48c6f4f449a147d7ccd5d13cee6c92e49ca8dcd
baszo <andplutecki@gmail.com>
2014-11-01 10:28:27 +0100

    poprawa

--- KiK/cross/src/cross/Plansza.java
+++ KiK/cross/src/cross/Plansza.java
@@ -1,72 +1,77 @@
-/**
- *
- * @author Andolinka
- */
+package cross;
+
 public class Plansza {
+	private Walidator walidator;
+	char wolne = '_';
+	static final int rozmiar = 3;
+	Znak[][] mojaPlansza = new Znak[rozmiar][rozmiar];
+	Znak znakPoczatkowy = new Znak(wolne);
+	
+	//nie wiem czy o to chodzi³o
+	
+	public Plansza(){
+		for (int i = 0; i < rozmiar; i++) {
+			for (int j = 0; j < rozmiar; j++) {
+				mojaPlansza[i][j]=znakPoczatkowy;
+			}
+		}
+		
+	}
+	
+	void ustawWalidator(Walidator walidator) {
+	this.walidator = walidator;
+	}
+	
+	void wypiszSiatke() {
+	//wypisuje aktualn¹ siatkê
+	for (int i = 0; i < rozmiar; i++) {
+	for (int j = 0; j < rozmiar; j++) {
+	System.out.print(mojaPlansza[i][j].pobierzZnak());
+	}
+	System.out.println();
+	}
+	}
+	char pobierzZnakZPlanszy(int kolumna, int wiersz) {
+		kolumna--;wiersz--;
+	return mojaPlansza[kolumna][wiersz].pobierzZnak();
+	}
+	boolean czyWolnePola() {//zwraca true jak s¹ dostêpne jakieœ wolne pola
+	int zliczPolaUzupelnione = 0;
+	for (int i = 0; i < rozmiar; i++) {
+	for (int j = 0; j < rozmiar; j++) {
+	if (mojaPlansza[i][j] != znakPoczatkowy) {
+	zliczPolaUzupelnione++;
+	}
+	}
+	}
+	return zliczPolaUzupelnione != rozmiar * rozmiar;
+	}
+	
+	boolean umiescZnak(int kolumna, int wiersz, Znak znak) {
+	//sprawdza czy mo¿e umieœciæ, jak mo¿e to umieszcza i zwraca true, jak nie mo¿e to false
+		if(kolumna >= rozmiar || wiersz >= rozmiar){
+			return false;
+		}
+	
+		if (mojaPlansza[kolumna][wiersz].pobierzZnak() == znakPoczatkowy.pobierzZnak()) {
+		mojaPlansza[kolumna][wiersz] = znak;
+		return true;
+		} 
+		else 
+		{
+			return false;
+		}
+	}
+	//boolean wolnePole(int kolumna, int wiersz) - to ustaliliœmy, ¿e ma byæ w klasie Znak, tak?
+	//pobiera aktualny rozmiar
+	int pobierzRozmiar() {
+	return rozmiar;
+	}
 
-    // static final char wolne = '_'; 
-    static final int rozmiar = 3;
-    Znak[][] mojaPlansza = new Znak[rozmiar - 1][rozmiar - 1];
-
-    Znak znak = new Znak();
-
-    Znak znakPoczatkowy = znak.wolnePole();//tu ma byÄ‡ przypisany nasz ustalony znak poczatkowy tj. ten wolny
-
-//nie wiem czy o to chodziÅ‚o
-    void ustawWalidator(Walidator walidator) {
-        walidator = new Walidator();
-    }
-
-    /*
-     void inicjalizujSiatkePoczatkowa(){
-     for (int i = 0; i < rozmiar; i++) {
-     for (int j = 0; j < rozmiar; j++) {
-     mojaPlansza[i][j]=znakPoczatkowy;
-     }
-     }
-     }
-     */
-     
-    void wypiszSiatke() {
-        //wypisuje aktualnÄ… siatkÄ™
-        for (int i = 0; i < rozmiar; i++) {
-            for (int j = 0; j < rozmiar; j++) {
-                System.out.print(mojaPlansza[i][j]);
-            }
-            System.out.println();
-        }
-    }
-
-    Znak pobierzZnakZPlanszy(int kolumna, int wiersz) {
-        return mojaPlansza[kolumna][wiersz];
-    }
-
-    boolean czyWolnePola() {//zwraca true jak sÄ… dostÄ™pne jakieÅ› wolne pola
-        int zliczPolaUzupelnione = 0;
-
-        for (int i = 0; i < rozmiar; i++) {
-            for (int j = 0; j < rozmiar; j++) {
-                if (mojaPlansza[i][j] != znakPoczatkowy) {
-                    zliczPolaUzupelnione++;
-                }
-            }
-        }
-        return zliczPolaUzupelnione != rozmiar * rozmiar;
-    }
-
-    boolean umiescZnak(int kolumna, int wiersz, Znak znak) {
-//sprawdza czy moÅ¼e umieÅ›ciÄ‡, jak moÅ¼e to umieszcza i zwraca true, jak nie moÅ¼e to false
-        if (mojaPlansza[kolumna][wiersz] == znakPoczatkowy) {
-            mojaPlansza[kolumna][wiersz] = znak;
-            return true;
-        } else {
-            return true;
-        }
-    }
-
-//boolean wolnePole(int kolumna, int wiersz)  - to ustaliliÅ›my, Å¼e ma byÄ‡ w klasie Znak, tak?
-//pobiera aktualny rozmiar    
-    int pobierzRozmiar() {
-        return rozmiar;
-    }
-}
+	public boolean wolnePole(int kolumna, int wiersz) {
+		if(mojaPlansza[kolumna][wiersz].pobierzZnak() == '_')
+			return true;
+		else
+			return false;
+	}
+	}
--- KiK/cross/src/cross/Walidator.java
+++ KiK/cross/src/cross/Walidator.java
@@ -1,29 +1,34 @@
+package cross;
 
 public class Walidator {
-
-	Plansza plansza= new Plansza();
+	
+	private Plansza plansza;
+	
+	public Walidator(Plansza plansza){
+		this.plansza=plansza;
+	}
 	
 	public boolean sprawdzRuch(int r, int c){
-		return plansza.wolnePole(r,c);		
+	return plansza.wolnePole(r,c);
 	}
-
 	public boolean czyWygrana(){
-		for ( int i=1; i<=3; i++){
-			if ((plansza.pobierzZnakZPlanszy(i,1)==plansza.pobierzZnakZPlanszy(i,2)) && (plansza.pobierzZnakZPlanszy(i,1)==plansza.pobierzZnakZPlanszy(i,3)) && (plansza.pobierzZnakZPlanszy(i,1)!='_')){
-				return true;
-			}
-		}
-		for ( int i=1; i<=3; i++){
-			if ((plansza.pobierzZnakZPlanszy(1,i)==plansza.pobierzZnakZPlanszy(2,i)) && (plansza.pobierzZnakZPlanszy(1,i)==plansza.pobierzZnakZPlanszy(3,i)) && (plansza.pobierzZnakZPlanszy(1,i)!='_')){
-				return true;
-			}
-		}
-		if ((plansza.pobierzZnakZPlanszy(1,1)==plansza.pobierzZnakZPlanszy(2,2)) && (plansza.pobierzZnakZPlanszy(1,1)==plansza.pobierzZnakZPlanszy(3,3)) && (plansza.pobierzZnakZPlanszy(2,2)!='_')){
-			return true;
-		}	
-		if ((plansza.pobierzZnakZPlanszy(3,1)==plansza.pobierzZnakZPlanszy(2,2)) && (plansza.pobierzZnakZPlanszy(3,1)==plansza.pobierzZnakZPlanszy(1,3)) && (plansza.pobierzZnakZPlanszy(2,2)!='_')){
-			return true;
-		}	
-		return false;
+	for ( int i=1; i<=3; i++){
+	if ((plansza.pobierzZnakZPlanszy(i,1)==plansza.pobierzZnakZPlanszy(i,2)) && (plansza.pobierzZnakZPlanszy(i,1)==plansza.pobierzZnakZPlanszy(i,3)) && (plansza.pobierzZnakZPlanszy(i,1)!='_')){
+	return true;
+	}
+	}
+	for ( int i=1; i<=3; i++){
+	if ((plansza.pobierzZnakZPlanszy(1,i)==plansza.pobierzZnakZPlanszy(2,i)) && (plansza.pobierzZnakZPlanszy(1,i)==plansza.pobierzZnakZPlanszy(3,i)) && (plansza.pobierzZnakZPlanszy(1,i)!='_')){
+	return true;
+	}
+	}
+	if ((plansza.pobierzZnakZPlanszy(1,1)==plansza.pobierzZnakZPlanszy(2,2)) && (plansza.pobierzZnakZPlanszy(1,1)==plansza.pobierzZnakZPlanszy(3,3)) && (plansza.pobierzZnakZPlanszy(2,2)!='_')){
+	return true;
+	}
+	if ((plansza.pobierzZnakZPlanszy(3,1)==plansza.pobierzZnakZPlanszy(2,2)) && (plansza.pobierzZnakZPlanszy(3,1)==plansza.pobierzZnakZPlanszy(1,3)) && (plansza.pobierzZnakZPlanszy(2,2)!='_')){
+	return true;
+	}
+	return false;
 	}
 }
+
